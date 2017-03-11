ceb6080261f7218a2b0ab8bc67f161d5c9170da0
doudouhuang <doudouhuang@Doudous-MBP.home>
2017-03-11 20:15:14 +0000

    solution

---
+++ Lesson6 Sorting/MaxProductOfThree.cpp
@@ -0,0 +1,25 @@
+/* 
+   To maximise A[P] * A[Q] * A[R] for any triplet (P, Q, R)
+   A positive number can only be obtained from + + + or - - +
+   Sort the array. Check the last three numbers. There are three cases:
+   1. A[n-1], A[n-2], A[n-3] all positive
+   2. A[n-1], A[n-2], A[n-3] all non-positive i.e. A[n-1] <= 0
+   3. otherwise
+*/
+
+#include <algorithm>
+
+int solution(vector<int> &A){
+    sort(A.begin(), A.end());
+    int n = A.size();
+    int a1 = A[n-1];
+    int a2 = A[n-2];
+    int a3 = A[n-3];
+    if(a3>0 && a2>0 && a1>0){
+        return a1*a2*a3 > A[0]*A[1]*a1? a1*a2*a3 : A[0]*A[1]*a1;
+    }
+    if(a1<=0){
+        return a1*a2*a3;
+    }
+    return A[0]*A[1]*a1;
+}
